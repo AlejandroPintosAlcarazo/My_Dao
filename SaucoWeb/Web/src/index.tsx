9b694ce49273c09bf156fe7be93c0e8c374f825e
alejandropintosalcarazo <alejandropintosdev@gmail.com>
2024-07-28 13:21:36 +0200

    Empezamos pro

--- SaucoWeb/Web/src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './styles/galileo-design1.module.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
